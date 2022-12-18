99d8fb2f219d7261c4dfb6780dc889464c14f680
андрей <70390276+smiling-sky@users.noreply.github.com>
2022-12-18 21:30:28 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+/*  видео 3 */
+
+// 1
+let colorEyes;
+colorEyes = 'blue';
+console.log(colorEyes);
+
+// 2
+let user;
+let userName;
+userName = 'Вася';
+
+user = userName;
+console.log(user);
